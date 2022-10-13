8b3cda782fb945f00400584cb05ca19c1dbfced7
Rimallasfcx111 <113599111+Rimallasfcx111@users.noreply.github.com>
2022-10-13 10:06:48 +0300

    pratimai

--- js/while.js
+++ js/while.js
@@ -1,3 +1,37 @@
 'use strict';
 console.log('while.js');
 
+let i = 1;
+// let sp = 'as esu while';
+// while (i < 11) {
+// console.log('as esu While');
+
+
+// i++;   
+// }  
+
+i =14;
+while (i < 35) {
+    console.log('i ===', i);
+    
+    i++;
+}
+
+i =0;
+while (i < 101) {
+    console.log('i');
+    // i = i + 3;
+    // arba
+    i += 3;
+    
+}
+
+i = 50;
+
+do {
+   
+   console.log('i');
+   
+    i--;
+
+}while(i > 24)
